6fd25bbdf4cc11eaca2977d943506e9cd501423c
ahernandezt <114923910+ahernandezt@users.noreply.github.com>
2022-10-27 11:57:40 -0300

    Update UserConfig.java

--- src/main/java/org/soyphea/k8s/config/UserConfig.java
+++ src/main/java/org/soyphea/k8s/config/UserConfig.java
@@ -6,13 +6,28 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.Cipher;
+
+
 @ConfigurationProperties(prefix = "user")
 @Component
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
+
+
+
 public class UserConfig {
+    
+    try
+        {
+            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+        }   
+    catch(Exception e)
+        {
+        }
+    
     String name;
     String blog;
 }
