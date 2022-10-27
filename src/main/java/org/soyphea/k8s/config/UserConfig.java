5b1d888e69b377436e9bace9a08f40c90e409287
ahernandezt <114923910+ahernandezt@users.noreply.github.com>
2022-10-27 10:56:56 -0300

    Update UserConfig.java

--- src/main/java/org/soyphea/k8s/config/UserConfig.java
+++ src/main/java/org/soyphea/k8s/config/UserConfig.java
@@ -6,13 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import org.h2.security.SHA256;
-
-String inputString = "s3cr37";
-byte[] key         = inputString.getBytes();
-
-SHA256.getHMAC(key, message);  // Noncompliant
-
 @ConfigurationProperties(prefix = "user")
 @Component
 @Data
